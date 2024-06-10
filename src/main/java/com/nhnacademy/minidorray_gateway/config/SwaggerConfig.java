a463157c6c783dbc10ef0e66a84fad2c4ae700bf
kihoo-ni <gudcjf532@gmail.com>
2024-06-10 09:22:34 +0900

    swagger 설정완료

--- src/main/java/com/nhnacademy/minidorray_gateway/config/SwaggerConfig.java
+++ src/main/java/com/nhnacademy/minidorray_gateway/config/SwaggerConfig.java
@@ -1,31 +1,6 @@
 package com.nhnacademy.minidorray_gateway.config;
 
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springdoc.core.models.GroupedOpenApi;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info().title("Task API").version("1.0").description("springdoc-openapi"));
-//    }
-//
-//    @Bean
-//    public GroupedOpenApi api() {
-//        String[] paths = {"/comments/**", "/milestones/**", "/tags/**", "/tasks/**", "/projects/", "/", "/api/**", "/auth", "/profile", "/register"};
-//        String[] packagesToScan = {"com.nhnacademy.minidorray_gateway"};
-//        return GroupedOpenApi.builder()
-//                .group("gateway-api")
-//                .pathsToMatch(paths)
-//                .packagesToScan(packagesToScan)
-//                .build();
-//    }
-//}
+
 
 
 
@@ -54,7 +29,7 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi api() {
         String[] paths = {"/**"};
-        String[] packagesToScan = {"com.nhnacademy.account_api"};
+        String[] packagesToScan = {"com.nhnacademy.minidorray_gateway"};
         return GroupedOpenApi.builder().group("springdoc-openapi")
                 .pathsToMatch(paths)
                 .packagesToScan(packagesToScan)
